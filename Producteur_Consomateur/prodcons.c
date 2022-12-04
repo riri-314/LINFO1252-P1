2dcf08e83fa28822488ac72d03fc8b22e8185fba
Henri Pihet <Henri Pihet henri.pihet@protonmail.com>
2022-12-04 15:31:12 +0100

    fixed division bug in prodcons.c

--- Producteur_Consomateur/prodcons.c
+++ Producteur_Consomateur/prodcons.c
@@ -15,8 +15,8 @@ int out = 0;
 
 void main(int argc, char *argv[]) {
     
-    int nb_consumers = atoi(argv[1]);
-    int nb_producers = atoi(argv[2]);
+    int nb_consumers = atoi(argv[2]);
+    int nb_producers = atoi(argv[1]);
     
     pthread_t cons[nb_consumers];
     pthread_t prod[nb_producers];
@@ -32,18 +32,47 @@ void main(int argc, char *argv[]) {
 
     int *to_produce = (int *)malloc(sizeof(int));
     *to_produce = 8192/nb_producers; //probleme avec la division TODO
+    //printf("to_produce before: %d\n", *to_produce);
+
+    int *to_produce_end = (int *)malloc(sizeof(int));
+    *to_produce_end = *to_produce + 8192 - (*to_produce * nb_producers);
+    //printf("to_produce_end: %d\n", *to_produce_end);
 
     for (int i = 0; i < nb_producers; i++)
     {
-        pthread_create(&prod[i], NULL, &producer, (void *) to_produce);
+        if (i == nb_producers-1)
+        {  
+            pthread_create(&prod[i], NULL, &producer, (void *) to_produce_end);
+            //printf("thread prod %d created with %d cycle to do\n", i, *to_produce_end);
+
+        }
+        else
+        {
+            pthread_create(&prod[i], NULL, &producer, (void *) to_produce);
+            //printf("thread prod %d created with %d cycle to do\n", i, *to_produce);
+        }
     }
 
     int *to_consume = (int *)malloc(sizeof(int));
     *to_consume = 8192/nb_consumers; //probleme avec la division TODO
+    //printf("to_consume before: %d\n", *to_consume);
+
+    int *to_consume_end = (int *)malloc(sizeof(int));
+    *to_consume_end = *to_consume + 8192 - (*to_consume * nb_consumers);
+   // printf("to_consume_end: %d\n", *to_consume_end);
 
     for (int i = 0; i < nb_consumers; i++)
     {
-        pthread_create(&cons[i], NULL, &consumer, (void *) to_consume);
+        if (i == nb_consumers-1)
+        {  
+            pthread_create(&cons[i], NULL, &consumer, (void *) to_consume_end);
+            //printf("thred cons %d created with %d cycle to do\n", i, *to_consume_end);
+        }
+        else
+        {
+            pthread_create(&cons[i], NULL, &consumer, (void *) to_consume);
+            //printf("thred cons %d created with %d cycle to do\n", i, *to_consume);
+        }
     }
 
     for (int i = 0; i < nb_producers; i++)
@@ -63,13 +92,14 @@ void *producer(void *arg) {
     {
         for (int j = 0; j < 10000; j++){
         }
+        //printf("Pre_Producing: %d\n", i);
         
         sem_wait(&empty);
         sem_wait(&mutex);
 
         buffer[in] = 1;
         in = (in+1)%8;
-        printf("Producing: %d\n", i);
+        //printf("Producing: %d\n", i);
 
         sem_post(&mutex);
         sem_post(&full);
@@ -88,7 +118,7 @@ void *consumer (void *arg) {
 
         buffer[out] = 0;
         out = (in+1)%8;
-        printf("consuming: %d\n", i);
+        //printf("consuming: %d\n", i);
 
         sem_post(&mutex);
         sem_post(&empty);
@@ -96,5 +126,6 @@ void *consumer (void *arg) {
         for (int j = 0; j < 10000; j++){
         }
     }
+    //printf("finish consuming\n");
     return (NULL);
 }
